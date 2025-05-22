657128b7637d7f5f15e3dc049ea3cdacc4fd7556
satheesh <sathisatheesh2843@gmail.com>
2025-05-22 11:04:08 +0530

    uploading

--- task/exceptionhandling_task01.java
+++ task/exceptionhandling_task01.java
@@ -1,7 +1,8 @@
 package task;
 
 import java.util.Scanner;
-public class exceptionhandling {
+
+public class exceptionhandling_task01 {
     static void divide(int a, int b){
         try{
             System.out.println(a/b);
@@ -55,6 +56,37 @@ public class exceptionhandling {
         System.out.println("the converting uppercase to lowercase is done !!");
 
 
+
+    }
+    static void multiplecatch(){
+        int a[]={12,123,432,5432,213,12,0,0,23,1};
+
+        int aa=12,b=0;
+        try{
+            System.out.println(aa/b);
+            System.out.println(a[100]);
+        }
+        catch (ArithmeticException bb){
+            System.out.println("dividing 0");
+        }
+        catch (ArrayIndexOutOfBoundsException cc){
+            System.out.println("array index out range");
+        }
+
+//        for(int i=0;i<a.length;i++){
+//            try{
+//                System.out.println(a[i]/a[i+1]);
+//            }
+//            catch (ArithmeticException aa){
+//                System.out.println("u dividing '0' to another variable !! check the value");
+//            }
+//            catch (ArrayIndexOutOfBoundsException bb){
+//                System.out.println("check the array size it excute out of range");
+//            }
+//            System.out.println();
+//        }
+
+
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -68,14 +100,15 @@ public class exceptionhandling {
 //            int a[]={1,2,3,4,5,6};
 //            sum(a);
 
-        //3. string to integer coversion error
+//        3. string to integer coversion error
 //        String s=sc.nextLine();
 //        strintt(s);
 
         //4. Null pointer exception
-        String str=null;
-        strin(str);
+//        String str=null;
+//        strin(str);
 
-        //5.
+        //5. Multiple catch block code;
+        multiplecatch();
     }
 }
